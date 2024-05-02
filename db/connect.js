fcd97ee4484dc6c20eb73381bded39cc2d549de8
Hiru1003 <hirumiperera10@gmail.com>
2024-05-02 18:05:30 +0530

    feat: resolving error

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {});
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(url, {});
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error; // Re-throw the error to be handled by the caller
+  }
 };
 
 module.exports = connectDB;

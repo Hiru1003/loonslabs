3c49a49e4d3408c2ec1fd84d5d43725a970fcc67
Hiru1003 <hirumiperera10@gmail.com>
2024-05-02 16:36:09 +0530

    Initial commit

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styles/Landing.css";
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <div className='landing-main'>
+    <h1>Dispensary Booking System</h1><br/>
+    <p>Hello and welcome!</p><br/>
+    <Link to="/login" className="landing-login-button">Login</Link>
+    <Link to="/register" className="landing-register-button">Register</Link>
+  </div>
+  )
+}
+
+export default Landing

7c5f871eb33da3dc1ae0f939fc09c0d60ba273f7
Hiru1003 <hirumiperera10@gmail.com>
2024-05-03 06:38:10 +0530

    Fix deployment issue: heroku

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 require('express-async-errors');
 
 const connectDB = require("./db/connect");
@@ -9,25 +8,20 @@ const app = express();
 const mainRouter = require("./routes/user");
 
 app.use(express.json());
-
-app.use(cors())
+app.use(cors());
 app.use("/api/v1", mainRouter);
 
 const port = process.env.PORT || 6000;
 
 const start = async () => {
-
-    try {        
+    try {
         await connectDB(process.env.MONGO_URI);
         app.listen(port, () => {
             console.log(`Server is listening on port ${port}`);
-        })
-
+        });
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
-}
+};
 
 start();
-
-//console-ninja node --watch app.js
